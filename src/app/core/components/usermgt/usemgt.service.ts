4c396e401fa803a7a2f9f1d4c20a88084dcc1ef1
Nisho <nisholin07@gmail.com7@gmail.com>
2020-08-22 19:57:58 +0530

    Alignment Change on user service

--- src/app/core/components/usermgt/usemgt.service.ts
+++ src/app/core/components/usermgt/usemgt.service.ts
@@ -5,37 +5,35 @@ import { Usermgt } from 'src/app/core/common/_models';
 @Injectable()
 export class UserMgtService {    
 
-public url = require("../apiurl.json");
-private commonURL = this.url[0].apiurl+'usermgt/';
+    public url = require("../apiurl.json");
+    private commonURL = this.url[0].apiurl+'usermgt/';
 
-constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-//*********************Employee************************
+    // Save 
+    save(usermgt: Usermgt){
+        console.log('service....');
+        return this.http.post<Usermgt>(this.commonURL+'save',usermgt);
+    }
 
-// Save 
-save(usermgt: Usermgt){
-    console.log('service....');
-    return this.http.post<Usermgt>(this.commonURL+'save',usermgt);
-}
+    // Load 
+    load(){
+        return this.http.get(this.commonURL+'load');
+    }
 
-// Load 
-load(){
-    return this.http.get(this.commonURL+'load');
-}
+    // get 
+    get(id:number){
+        return this.http.get<Usermgt>(this.commonURL+'get?id='+id);
+    }
 
-// get 
-get(id:number){
-    return this.http.get<Usermgt>(this.commonURL+'get?id='+id);
-}
+    // Update 
+    update(usermgt: Usermgt){
+        return this.http.put<Usermgt>(this.commonURL+'update',usermgt);
 
-// Update 
-update(usermgt: Usermgt){
-    return this.http.put<Usermgt>(this.commonURL+'update',usermgt);
+    }
 
-}
-
-// Remove 
-remove(id:number){
-    return this.http.delete<String>(this.commonURL+'remove?id='+id);
-}
+    // Remove 
+    remove(id:number){
+        return this.http.delete<String>(this.commonURL+'remove?id='+id);
+    }
 }
