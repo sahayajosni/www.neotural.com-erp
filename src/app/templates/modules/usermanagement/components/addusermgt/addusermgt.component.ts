e75b8e5c01f603393c532f1bd623cddc9b2a88fc
Nisho <nisholin07@gmail.com7@gmail.com>
2020-08-24 20:18:25 +0530

    Add UserMgt Changes

--- src/app/templates/modules/usermanagement/components/addusermgt/addusermgt.component.ts
+++ src/app/templates/modules/usermanagement/components/addusermgt/addusermgt.component.ts
@@ -33,16 +33,12 @@ export class AddUserMgtComponent implements OnInit {
   public sales = false;
 
   passwordtype = "password";
-  purchaseArray: Array<any> = [];
-  prodArray: Array<any> = [];
-  salesArray: Array<any> = [];
-  
-  menuArray: Array<any> = [];
+  public validationMsg = false;
+
   userArray: Array<any> = [];
 
   constructor(
     private router: Router,
-    //public dialogRef: MatDialogRef<AddUserMgtComponent>,
     private snackBar: MatSnackBar,
     private userMgtService: UserManagementService,
     public activeModal: NgbActiveModal,
@@ -53,6 +49,7 @@ export class AddUserMgtComponent implements OnInit {
 
   ngOnInit() {
     this.model.password = "";
+    this.validationMsg = false;
     this.userMgtService.loadDepartment().subscribe(
       data => { 
         this.departmentList = data;
@@ -119,55 +116,141 @@ export class AddUserMgtComponent implements OnInit {
     this.model.menuItem3 = 'MEN0';
     this.model.menuItem7 = 'MEN0';
     this.model.menuItem8 = 'MEN0';
+
+    this.model.purchasesubmenu1 = 'SUBMEN0';
+    this.model.purchasesubmenu2 = 'SUBMEN0';
+    this.model.purchasesubmenu3 = 'SUBMEN0';
+    this.model.productsubmenu1 = 'SUBMEN0';
+    this.model.productsubmenu2 = 'SUBMEN0';
+    this.model.productsubmenu3 = 'SUBMEN0';
+    this.model.salessubmenu1 = 'SUBMEN0';
+    this.model.salessubmenu2 = 'SUBMEN0';
+    this.model.salessubmenu3 = 'SUBMEN0';
+    this.model.salessubmenu4 = 'SUBMEN0';
+    this.model.salessubmenu5 = 'SUBMEN0';
   }
 
   onPurchaseSelect(puritem: any) {
     console.log('Purchase SingleSelect  -->'+puritem.purchase_item_text+" ID -->"+puritem.purchase_item_id);
-    this.purchaseArray.push(puritem.purchase_item_text);
+    this.validationMsg = false;
+    if(puritem.purchase_item_id == 'SUBMEN10001'){
+      this.model.purchasesubmenu1 = 'SUBMEN10001';
+    }else if(puritem.purchase_item_id == 'SUBMEN10002'){
+      this.model.purchasesubmenu2 = 'SUBMEN10002';
+    }else if(puritem.purchase_item_id == 'SUBMEN10003'){
+      this.model.purchasesubmenu3 = 'SUBMEN10003';
+    }
   }
   onPurSelectAll(puritems: any) {
-    this.purchaseArray = [];
-    this.purchaseArray.push("Orders","Invoices","Returns");
-    //this.purchaseArray.push("SUBMEN10001","SUBMEN10002","SUBMEN10003");
+    this.model.purchasesubmenu1 = 'SUBMEN10001';
+    this.model.purchasesubmenu2 = 'SUBMEN10002';
+    this.model.purchasesubmenu3 = 'SUBMEN10003';
   }
 
-  onDeSelectPurchase(items: any){
-    this.purchaseArray = [];
+  onDeSelectAllPurchase(items: any){
+    this.model.purchasesubmenu1 = 'SUBMEN0';
+    this.model.purchasesubmenu2 = 'SUBMEN0';
+    this.model.purchasesubmenu3 = 'SUBMEN0';
+  }
+
+  onDeSelectPurchase(puritem: any) {
+    console.log('Purchase Single DeSelect  -->'+puritem.purchase_item_id);
+    this.model.number = puritem.purchase_item_id;
+    if(this.model.number == 'SUBMEN10001'){
+      this.model.purchasesubmenu1 = 'SUBMEN0';
+    }else if(this.model.number == 'SUBMEN10002'){
+      this.model.purchasesubmenu2 = 'SUBMEN0';
+    }else if(this.model.number == 'SUBMEN10003'){
+      this.model.purchasesubmenu3 = 'SUBMEN0';
+    }
   }
 
   onProductSelect(proditem: any) {
+    this.validationMsg = false;
     console.log('Product SingleSelect  -->'+proditem.product_item_text+" ID -->"+proditem.product_item_id);
-    this.prodArray.push(proditem.product_item_text);
+    if(proditem.product_item_id == 'SUBMEN10004'){
+      this.model.productsubmenu1 = 'SUBMEN10004';
+    }else if(proditem.product_item_id == 'SUBMEN10005'){
+      this.model.productsubmenu2 = 'SUBMEN10005';
+    }else if(proditem.product_item_id == 'SUBMEN10006'){
+      this.model.productsubmenu3 = 'SUBMEN10006';
+    }
   }
 
   onprodSelectAll(proditems: any) {
-    this.prodArray = [];
-    this.prodArray.push("product","units","category");
-    //this.prodArray.push("SUBMEN10004","SUBMEN10005","SUBMEN10006");
+    this.model.productsubmenu1 = 'SUBMEN10004';
+    this.model.productsubmenu2 = 'SUBMEN10005';
+    this.model.productsubmenu3 = 'SUBMEN10006';
   }
 
-  onDeSelectProduct(items: any){
-    this.prodArray = [];
+  onDeSelectAllProduct(items: any){
+    this.model.productsubmenu1 = 'SUBMEN0';
+    this.model.productsubmenu2 = 'SUBMEN0';
+    this.model.productsubmenu3 = 'SUBMEN0';
+  }
+
+  onDeSelectProduct(proditem: any) {
+    console.log('Product Single DeSelect  -->'+proditem.product_item_id);
+    this.model.number = proditem.product_item_id;
+    if(this.model.number == 'SUBMEN10004'){
+      this.model.productsubmenu1 = 'SUBMEN0';
+    }else if(this.model.number == 'SUBMEN10005'){
+      this.model.productsubmenu2 = 'SUBMEN0';
+    }else if(this.model.number == 'SUBMEN10006'){
+      this.model.productsubmenu3 = 'SUBMEN0';
+    }
   }
 
   onSalesSelect(item: any) {
-    console.log('Product SingleSelect  -->'+item.sales_item_text+" ID -->"+item.sales_item_id);
-    this.salesArray.push(item.sales_item_text);
+    this.validationMsg = false;
+    console.log('Sales SingleSelect  -->'+item.sales_item_id);
+    if(item.sales_item_id == 'SUBMEN10007'){
+      this.model.salessubmenu1 = 'SUBMEN10007';
+    }else if(item.sales_item_id == 'SUBMEN10008'){
+      this.model.salessubmenu2 = 'SUBMEN10008';
+    }else if(item.sales_item_id == 'SUBMEN10009'){
+      this.model.salessubmenu3 = 'SUBMEN10009';
+    }else if(item.sales_item_id == 'SUBMEN10010'){
+      this.model.salessubmenu4 = 'SUBMEN10010';
+    }else  if(item.sales_item_id == 'SUBMEN10011'){
+      this.model.salessubmenu5 = 'SUBMEN10011';
+    }
   }
 
   onSalesAll(items: any) {
-    this.salesArray = [];
-    this.salesArray.push("Orders","Invoices","Customer","Returns","Promotion");
-    //this.salesArray.push("SUBMEN10007","SUBMEN10008","SUBMEN10009","SUBMEN10010","SUBMEN10011");
+    this.model.salessubmenu1 = 'SUBMEN10007';
+    this.model.salessubmenu2 = 'SUBMEN10008';
+    this.model.salessubmenu3 = 'SUBMEN10009';
+    this.model.salessubmenu4 = 'SUBMEN10010';
+    this.model.salessubmenu5 = 'SUBMEN10011';
   }
 
   onDeSalesAll(items: any){
-    this.salesArray = [];
+    this.model.salessubmenu1 = 'SUBMEN0';
+    this.model.salessubmenu2 = 'SUBMEN0';
+    this.model.salessubmenu3 = 'SUBMEN0';
+    this.model.salessubmenu4 = 'SUBMEN0';
+    this.model.salessubmenu5 = 'SUBMEN0';
+  }
+
+  onSalesDeSelect(item: any) {
+    console.log('Sales Single DeSelect  -->'+item.sales_item_id);
+    this.model.number = item.sales_item_id;
+    if(this.model.number == 'SUBMEN10007'){
+      this.model.salessubmenu1 = 'SUBMEN0';
+    }else if(this.model.number == 'SUBMEN10008'){
+      this.model.salessubmenu2 = 'SUBMEN0';
+    }else if(this.model.number == 'SUBMEN10009'){
+      this.model.salessubmenu3 = 'SUBMEN0';
+    }else if(this.model.number == 'SUBMEN10010'){
+      this.model.salessubmenu4 = 'SUBMEN0';
+    }else if(this.model.number == 'SUBMEN10011'){
+      this.model.salessubmenu5 = 'SUBMEN0';
+    }
   }
 
   getPurchase(isPurchaseChecked: boolean){
     console.log("Is Purchase Check Boolean --->"+isPurchaseChecked);
-    this.purchaseArray = [];
     if (isPurchaseChecked) {
       this.purchase = true;
     }else{
@@ -176,7 +259,6 @@ export class AddUserMgtComponent implements OnInit {
   }
 
   getProduct(isProductChecked: boolean){
-    this.prodArray = [];
     if (isProductChecked) {
       this.product = true;
     }else{
@@ -185,7 +267,6 @@ export class AddUserMgtComponent implements OnInit {
   }
 
   getSales(isSalesChecked: boolean){
-    this.salesArray = [];
     if (isSalesChecked) {
       this.sales = true;
     }else{
@@ -194,6 +275,7 @@ export class AddUserMgtComponent implements OnInit {
   }
 
   getMenu1(menu1){
+    this.validationMsg = false;
     if(menu1 == true){
       this.model.menuItem1 = 'MEN10000';
     }else if(menu1 == false){
@@ -203,6 +285,7 @@ export class AddUserMgtComponent implements OnInit {
   }
 
   getMenu2(menu2){
+    this.validationMsg = false;
     if(menu2 == true){
       this.model.menuItem2 = 'MEN10001';
     }else if(menu2 == false){
@@ -212,6 +295,7 @@ export class AddUserMgtComponent implements OnInit {
   }
 
   getMenu3(menu3){
+    this.validationMsg = false;
     if(menu3 == true){
       this.model.menuItem3 = 'MEN10002';
     }else if(menu3 == false){
@@ -221,6 +305,7 @@ export class AddUserMgtComponent implements OnInit {
   }
 
   getMenu7(menu7){
+    this.validationMsg = false;
     if(menu7 == true){
       this.model.menuItem7 = 'MEN10006';
     }else if(menu7 == false){
@@ -230,6 +315,7 @@ export class AddUserMgtComponent implements OnInit {
   }
 
   getMenu8(menu8){
+    this.validationMsg = false;
     if(menu8 == true){
       this.model.menuItem8 = 'MEN10007';
     }else if(menu8 == false){
@@ -248,23 +334,24 @@ export class AddUserMgtComponent implements OnInit {
     console.log("Menu Name7 -->"+this.model.menuItem7);
     console.log("Menu Name8 -->"+this.model.menuItem8);
     console.log("Menu Name9 -->"+this.model.menuItem9);
-    console.log("Purchase Sub Name -->"+this.purchaseArray.length);
-    console.log("Product Sub Name -->"+this.prodArray.length);
-    console.log("Sales Name -->"+this.salesArray.length);  
 
-    if(this.purchaseArray.length > 0){
+    if(this.model.purchasesubmenu1 == 'SUBMEN10001' || this.model.purchasesubmenu2 == 'SUBMEN10002' ||
+      this.model.purchasesubmenu3 == 'SUBMEN10003'){
       this.model.menuItem4 = 'MEN10003';
     }else{
       this.model.menuItem4 = 'MEN0';
     }
     
-    if(this.prodArray.length > 0){
+    if(this.model.productsubmenu1 == 'SUBMEN10004' || this.model.productsubmenu2 == 'SUBMEN10005' ||
+      this.model.productsubmenu3 == 'SUBMEN10006'){
       this.model.menuItem5 = 'MEN10004';
     }else{
       this.model.menuItem5 = 'MEN0';
     }
 
-    if(this.salesArray.length > 0){
+    if(this.model.salessubmenu1 == 'SUBMEN10007' || this.model.salessubmenu2 == 'SUBMEN10008' ||
+      this.model.salessubmenu3 == 'SUBMEN10009' || this.model.salessubmenu4 == 'SUBMEN10010' || 
+      this.model.salessubmenu5 == 'SUBMEN10011'){
       this.model.menuItem6 = 'MEN10005';
     }else{
       this.model.menuItem6 = 'MEN0';
@@ -273,40 +360,33 @@ export class AddUserMgtComponent implements OnInit {
     console.log("Menu Name4 -->"+this.model.menuItem4);
     console.log("Menu Name5 -->"+this.model.menuItem5);
     console.log("Menu Name6 -->"+this.model.menuItem6);
-
-    this.menuArray.push( this.model.menuItem1,this.model.menuItem2,this.model.menuItem3,this.model.menuItem4,
-      this.purchaseArray,this.model.menuItem5,this.prodArray,this.model.menuItem6,this.salesArray, 
-      this.model.menuItem7,this.model.menuItem8 );
-
-      var filteredarray = this.menuArray.filter(function (el) {
-        return el != null;
-      }); 
-      
-      console.log(filteredarray);
     
-    /* this.userArray.push({ username : this.model.username,password : this.model.password,
-      departmentname:this.model.departmentname,menuArray: filteredarray }); */
+    if(this.model.menuItem1 == 'MEN0' && this.model.menuItem2 == 'MEN0' && this.model.menuItem3 == 'MEN0' && 
+        this.model.menuItem7 == 'MEN0' && this.model.menuItem8 == 'MEN0' && this.model.menuItem4 == 'MEN0' && 
+        this.model.menuItem5 == 'MEN0' && this.model.menuItem6 == 'MEN0'){
+
+        this.validationMsg = true;
+
     
-      if(this.model.menuItem1 == 'MEN0' && this.model.menuItem2 == 'MEN0' && this.model.menuItem3 == 'MEN0' && 
-        this.model.menuItem7 == 'MEN0' && this.model.menuItem8 == 'MEN0' && this.purchaseArray.length == 0 && 
-        this.prodArray.length == 0 && this.salesArray.length == 0){
-      
-        this.snackBar.open(
-          "Please Choose atleast one Menu",
-          "",
-          {
-            panelClass: ["warning"],
-            verticalPosition: "top",
-          }
-        );
+      /* this.snackBar.open(
+        "Please Choose atleast one Menu",
+        "",
+        {
+          panelClass: ["warning"],
+          verticalPosition: "top",
+        }
+      ); */
 
     }else{
       
-      this.userArray.push({ username : this.model.username,password : this.model.password,menuArray: filteredarray,
+      this.userArray.push({ username : this.model.username,password : this.model.password,
         departmentname:this.model.departmentname,menuItem1 : this.model.menuItem1,menuItem2 : this.model.menuItem2,
-        menuItem3 : this.model.menuItem3, menuItem4 : this.model.menuItem4,purchasesubmenu : this.purchaseArray,
-        menuItem5 : this.model.menuItem5,productsubmenu : this.prodArray, menuItem6 : this.model.menuItem6,
-        salessubmenu : this.salesArray, menuItem7 : this.model.menuItem7, menuItem8 : this.model.menuItem8 }); 
+        menuItem3 : this.model.menuItem3, menuItem4 : this.model.menuItem4,purchasesubmenu1 : this.model.purchasesubmenu1,
+        purchasesubmenu2 : this.model.purchasesubmenu2,purchasesubmenu3 : this.model.purchasesubmenu3,menuItem5 : this.model.menuItem5, 
+        productsubmenu1 : this.model.productsubmenu1,productsubmenu2 : this.model.productsubmenu2,productsubmenu3 : this.model.productsubmenu3,
+        menuItem6 : this.model.menuItem6,salessubmenu1 : this.model.salessubmenu1,salessubmenu2 : this.model.salessubmenu2,
+        salessubmenu3 : this.model.salessubmenu3,salessubmenu4 : this.model.salessubmenu4,salessubmenu5 : this.model.salessubmenu5,
+        menuItem7 : this.model.menuItem7, menuItem8 : this.model.menuItem8 }); 
 
       this.userMgtService.save(this.userArray)
       .subscribe(
@@ -325,10 +405,6 @@ export class AddUserMgtComponent implements OnInit {
     
       });
 
-      this.menuArray = [];
-      this.purchaseArray = [];
-      this.prodArray = [];
-      this.salesArray = [];
     }
   }
 
