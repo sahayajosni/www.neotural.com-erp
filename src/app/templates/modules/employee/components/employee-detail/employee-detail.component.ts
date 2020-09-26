374db6c34d6def48fc36e95700cbb1aa5ce5d920
josni <shahayajoseny@gmail.com>
2020-09-26 15:47:41 +0530

    Employee details design changes

--- src/app/templates/modules/employee/components/employee-detail/employee-detail.component.ts
+++ src/app/templates/modules/employee/components/employee-detail/employee-detail.component.ts
@@ -226,11 +226,11 @@ export class EmployeeDetailComponent implements OnInit {
   }
 
   printDiv(divName) {
-    var printContents = document.getElementById(divName).innerHTML;
+    /* var printContents = document.getElementById(divName).innerHTML;
     var originalContents = document.body.innerHTML;
     document.body.innerHTML = printContents;
     window.print();
-    document.body.innerHTML = originalContents;
+    document.body.innerHTML = originalContents; */
   }
 
 
