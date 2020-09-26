6376795b29aa31e157e6ed296244a79eac36e9e7
josni <shahayajoseny@gmail.com>
2020-09-26 13:54:02 +0530

    remove tooltip in add employee

--- src/app/templates/modules/employee/components/employee-detail/employee-detail.component.ts
+++ src/app/templates/modules/employee/components/employee-detail/employee-detail.component.ts
@@ -225,5 +225,13 @@ export class EmployeeDetailComponent implements OnInit {
 		); 
   }
 
+  printDiv(divName) {
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+  }
+
 
 }
